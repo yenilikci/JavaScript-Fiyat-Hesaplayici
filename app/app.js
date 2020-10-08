e021785263784a0068d3020989f483218d08d5a6
yenilikci <melihcelik017@outlook.com>
2020-10-08 15:33:52 +0300

    Data Yapısı Oluşturuldu

--- app/app.js
+++ app/app.js
@@ -7,10 +7,32 @@ const StorageControler = (function(){
 const ProductController = (function(){
 
     //private
+    const Product = function(id,name,price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    const data = {
+        //ürünler dizisi içerisinde ürün objeleri tutuyor
+        products: [
+            {id:0,name:'Monitor',price:100},
+            {id:1,name:'Ram',price:100},
+            {id:2,name:'Mouse',price:100},
+        ],
+        //seçilen ürünü tutar
+        selectedProduct:null,
+        totalPrice:0  //fiyat toplamları başlangıçta sıfır
+    }
 
     //public
     return{
-        
+        getProducts : function(){
+            return data.products; //data nesnesi içerisindeki ürünleri geri döndürür
+        },
+        getData : function(){
+            return data; //data nesnesini geri döndürür
+        }
     }
 
 })(); 
@@ -20,8 +42,16 @@ const UIController = (function(){
 
 })();
 
-// App Controller Modülü
-const AppController = (function(ProductCtrl,UICtrl) //beklenen parametreler
+// App Modülü
+const App = (function(ProductCtrl,UICtrl) //beklenen parametreler
 {
-
+    return{
+        init : function(){
+            console.log('uygulama başlatılıyor...');
+            const products = ProductCtrl.getProducts(); //ürünleri getirdim
+            UICtrl.createProductList(products); //UICtrl üzerinde oluşturulacak createProductList metodu bizden ürün bilgilerini alıp index içerisindeki ürün tablosuna yerleştirecek
+        } 
+    }
 })(ProductController,UIController); //parametre değerleri
+
+App.init();
