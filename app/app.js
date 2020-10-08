18532fc8c80e877e994811381349c394b7c8754c
yenilikci <melihcelik017@outlook.com>
2020-10-08 15:21:44 +0300

    Modüllerin Oluşturulması

---
+++ app/app.js
@@ -0,0 +1,27 @@
+// Storage Controller Modülü
+const StorageControler = (function(){
+
+})();
+
+// Product Controller Modülü
+const ProductController = (function(){
+
+    //private
+
+    //public
+    return{
+        
+    }
+
+})(); 
+
+// UI Controller Modülü
+const UIController = (function(){
+
+})();
+
+// App Controller Modülü
+const AppController = (function(ProductCtrl,UICtrl) //beklenen parametreler
+{
+
+})(ProductController,UIController); //parametre değerleri
